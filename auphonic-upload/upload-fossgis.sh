f38819e3400346e98b787762d755cac1c3c994aa
VOC User <voc@relay.voc.lan>
2014-03-20 12:17:58 +0100

    fix filename filter

--- auphonic-upload/upload-fossgis.sh
+++ auphonic-upload/upload-fossgis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "uploading mp4s"
-./upload.py --schedule http://www.fossgis.de/konferenz/2014/programm/schedule.de.xml --recordings /video/fossgis14/ --extension mp4 --finished /video/fossgis14/uploaded/ --auphonic-login ./login --auphonic-preset PgeXpchzojrrgM25vVPw3U
+./upload.py --schedule http://www.fossgis.de/konferenz/2014/programm/schedule.de.xml --recordings /video/fossgis14/ --extension .mp4 --finished /video/fossgis14/uploaded/ --auphonic-login ./login --auphonic-preset PgeXpchzojrrgM25vVPw3U
 
 echo "uploading webms"
-./upload.py --schedule http://www.fossgis.de/konferenz/2014/programm/schedule.de.xml --recordings /video/fossgis14/ --extension webm --finished /video/fossgis14/uploaded/ --auphonic-login ./login --auphonic-preset hqvsm8SCL3QmCYkCDhbm7T
+./upload.py --schedule http://www.fossgis.de/konferenz/2014/programm/schedule.de.xml --recordings /video/fossgis14/ --extension .webm --finished /video/fossgis14/uploaded/ --auphonic-login ./login --auphonic-preset hqvsm8SCL3QmCYkCDhbm7T
