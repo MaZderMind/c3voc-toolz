7fc148bff1ee9cbee4a3596b2704c54980c1e8a4
MaZderMind <github@mazdermind.de>
2014-04-04 15:47:42 +0200

    generate thumbs. and candy.

--- recordings/lib/ThumbGenerator.rb
+++ recordings/lib/ThumbGenerator.rb
@@ -1,10 +1,12 @@
+require 'shellwords'
+
 class ThumbGenerator < Nanoc::Filter
 	identifier :generate_thumb
 	type :text => :binary
 
-	def run(content, params={})
-		File.open(output_filename, 'w') do |file|
-			file.write("png binary thumb")
-		end
+	def run(url, params={})
+		puts "\tgenerating thumbnail\t".green+" #{url}"
+
+		system("avconv -i #{url.shellescape} -ss 20 -frames:v 1 -vf scale='iw*sar:ih' -f image2 -c png #{output_filename.shellescape} >/dev/null 2>&1")
 	end
 end
